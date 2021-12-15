c352f5c663d90c3262662f581bcbb42462d548fd
NicholasK <nicholask1@outlook.com>
2021-12-14 19:09:26 -0500

    Created route to generate.html and added requred logic

--- app.py
+++ app.py
@@ -12,23 +12,51 @@ def get_madlib():
 
 @app.route("/story")
 def get_story():
-    place = request.args["place"]
-    noun = request.args["noun"]
-    verb = request.args["verb"]
-    adjective = request.args["adjective"]
-    plural_noun = request.args["plural_noun"]
 
-    words = {"place": place,
-             "noun": noun,
-             "verb": verb,
-             "adjective": adjective,
-             "plural_nuon": plural_noun
-             }
+    if "story" in request.args:
+        place = request.args["place"]
+        noun = request.args["noun"]
+        verb = request.args["verb"]
+        adjective = request.args["adjective"]
+        plural_noun = request.args["plural_noun"]
+        story = request.args["story"]
 
-    story = stories.Story(
-        ["place", "noun", "verb", "adjective", "plural_noun"],
-        """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-    )
+        words = {"place": place,
+                 "noun": noun,
+                 "verb": verb,
+                 "adjective": adjective,
+                 "plural_noun": plural_noun,
+                 "story": story
+                 }
 
-    madlib = story.generate(words)
+        gen_story = stories.Story(
+            ["place", "noun", "verb", "adjective", "plural_noun"], story)
+
+        madlib = gen_story.generate(words)
+        return render_template("story.html", madlib=madlib)
+    else:
+        place = request.args["place"]
+        noun = request.args["noun"]
+        verb = request.args["verb"]
+        adjective = request.args["adjective"]
+        plural_noun = request.args["plural_noun"]
+
+        words = {"place": place,
+                 "noun": noun,
+                 "verb": verb,
+                 "adjective": adjective,
+                 "plural_noun": plural_noun,
+                 }
+
+        story = stories.Story(
+            ["place", "noun", "verb", "adjective", "plural_noun"],
+            """Once upon a time in a long-ago {place}, there lived a
+            large {adjective} {noun}. It loved to {verb} {plural_noun}."""
+        )
+        madlib = story.generate(words)
+        return render_template("story.html", madlib=madlib)
+
+
+@app.route("/generate")
+def make_story():
+    return render_template("generate.html")
